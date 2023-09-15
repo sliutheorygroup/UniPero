d9a5d80d8083180623845f99f732f6390ce6e008
sliutheorygroup <66986486+sliutheorygroup@users.noreply.github.com>
2023-09-15 14:24:32 +0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -3,13 +3,13 @@
 cd model
 mv *.pb graph.pb
 cd ..
-cp model/graph.pb data/TaskI/script
+cp model/graph.pb data/Task-I/script
 cp model/graph.pb data/fitting/script
 
 for i in 300K 450K 900K
 do 
-     cp model/graph.pb data/TaskII/$i/script
-     cp model/graph.pb data/TaskII/$i/dptest
+     cp model/graph.pb data/Task-II/$i/script
+     cp model/graph.pb data/Task-II/$i/dptest
 done
 
 for i in BT PT ST KN KNN PST PZT a-PIN_PMN_PT b-PIN_PMN_PT
@@ -21,8 +21,8 @@ done
 #copy json files
 for i in 300K 450K 900K
 do 
-     cp json/machine.json data/TaskII/$i/script
-     cp json/job.json data/TaskII/$i/script
+     cp json/machine.json data/Task-II/$i/script
+     cp json/job.json data/Task-II/$i/script
 done
 
 for i in BT PT ST KN KNN PST PZT a-PIN_PMN_PT b-PIN_PMN_PT
